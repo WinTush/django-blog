1402879a7157e44a2beffdae5a066c090f963813
Winston Kinoti <prog.kinoti@gmail.com>
2023-08-01 18:18:58 +0300

    Formatted code using black and isort

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,7 @@
-from django.db import models
 from django.conf import settings
 from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
 from django.contrib.contenttypes.models import ContentType
+from django.db import models
 
 
 class Tag(models.Model):
