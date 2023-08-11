157c3613e2f1ac78d4e6b3ea83d9823876d9e01c
Winston Kinoti <prog.kinoti@gmail.com>
2023-08-12 02:37:20 +0300

    Add django_browser_reload during development

--- blango/settings.py
+++ blango/settings.py
@@ -31,15 +31,27 @@ ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
-INSTALLED_APPS = [
+DJANGO_APPS = [
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+]
+
+THIRD_PARTY_APPS = []
+
+LOCAL_APPS = [
     "blog",
 ]
+
+if DEBUG:
+    THIRD_PARTY_APPS += [
+        "django_browser_reload",
+    ]
+
+INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
@@ -50,6 +62,11 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+if DEBUG:
+    MIDDLEWARE += [
+        "django_browser_reload.middleware.BrowserReloadMiddleware",
+    ]
 
 ROOT_URLCONF = "blango.urls"
 
