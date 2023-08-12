a48e64491a270830502fb6578fa76df355b9758b
Winston Kinoti <prog.kinoti@gmail.com>
2023-08-13 02:22:28 +0300

    Added url namespace for the blog app

--- blango/urls.py
+++ blango/urls.py
@@ -3,6 +3,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("blog.urls")),
+    path("", include("blog.urls", namespace="blog")),
     path("__reload__/", include("django_browser_reload.urls")),
 ]
