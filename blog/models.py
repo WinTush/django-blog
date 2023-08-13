14ea1b4134837c355f77599fac05f6762bd21523
Winston Kinoti <prog.kinoti@gmail.com>
2023-08-13 18:00:22 +0300

    Modify Post model to automatically generate slug based on title

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
 from django.contrib.contenttypes.models import ContentType
 from django.db import models
+from django.utils.text import slugify
 
 
 class Tag(models.Model):
@@ -35,3 +36,8 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        return super().save(*args, **kwargs)
