69a12f3896e60b8796833a02d36cb956e9b6e52f
Winston Kinoti <prog.kinoti@gmail.com>
2023-08-13 18:45:39 +0300

    Display `me` if current user is post author

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from django import template
 from django.contrib.auth import get_user_model
 from django.contrib.auth.models import User
@@ -8,9 +10,12 @@ register = template.Library()
 
 
 @register.filter
-def author_details(author: User):
+def author_details(author: User, current_user: Optional[User] = None):
     if not isinstance(author, user_model):
         return ""
+
+    if author == current_user:
+        return format_html("<strong>me</strong>")
 
     if author.first_name and author.last_name:
         name = f"{author.first_name} {author.last_name}"
