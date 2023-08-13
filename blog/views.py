8148a00f2e5cb53fe15132ed25b8ff65098391e5
Winston Kinoti <prog.kinoti@gmail.com>
2023-08-13 17:56:48 +0300

    Display published blog posts

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,10 @@
 from django.http import HttpRequest
 from django.shortcuts import render
+from django.utils import timezone
+
+from .models import Post
 
 
 def index(request: HttpRequest):
-    return render(request, "blog/index.html")
+    posts = Post.objects.filter(published_at__lte=timezone.now())
+    return render(request, "blog/index.html", {"posts": posts})
